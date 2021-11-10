3e7be6764064aec6e5840095791b02244aef32b3
xingdaliunc <xingdali@live.unc.edu>
2021-11-10 11:35:03 -0500

    add comments

--- node_server/src/index.js
+++ node_server/src/index.js
@@ -2,9 +2,12 @@ const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 8000 });
 
+// Adds a listener function to the listener array for the event name "connection".
 wss.on("connection", async (ws) => {
   console.log("New client connected");
+  // Wait for 0.5s
   await new Promise(r => setTimeout(r, 500));
+  // Send message "20% of the files are processed" to the client side.
   ws.send(20)
   await new Promise(r => setTimeout(r, 1000));
   ws.send(40)
